76912a8b4dc8ff348ab7816ffbbc507b7003278b
Bjoern <malzacher.bjoern@gmail.com>
2025-05-08 15:22:48 +0200

    optimazation and visualization

--- train_model.py
+++ train_model.py
@@ -4,19 +4,23 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 from torch import Tensor
+import math
+import os
 
 from ax.service.ax_client import AxClient, ObjectiveProperties
 from ax.utils.notebook.plotting import render
+from ax.utils.report.render import render_report_elements
 from ax.modelbridge.generation_strategy import GenerationStep, GenerationStrategy
 from ax.modelbridge.registry import Models
 from ax.models.torch.botorch_modular.model import BoTorchModel
 from ax.models.torch.botorch_modular.surrogate import SurrogateSpec
 from ax.models.torch.botorch_modular.utils import ModelConfig
 from ax.plot.trace import optimization_trace_single_method
-
+from plotly.offline import plot
 from botorch.acquisition.acquisition import AcquisitionFunction
 from botorch.acquisition.input_constructors import acqf_input_constructor, MaybeDict
 from botorch.acquisition.logei import qLogExpectedImprovement, qLogNoisyExpectedImprovement
+from botorch.acquisition.analytic import UpperConfidenceBound, ProbabilityOfImprovement
 from botorch.models import SingleTaskGP
 from botorch.models.model import Model
 from botorch.utils.datasets import SupervisedDataset
@@ -24,12 +28,14 @@ from ax.plot.contour import plot_contour
 from ax.plot.trace import optimization_trace_single_method
 import plotly.express as px
 import pandas as pd
+from ax.plot.contour import interact_contour
 import numpy as np
 import plotly.graph_objects as go
 from ax.plot.trace import optimization_trace_single_method_plotly
 from ax.modelbridge.factory import get_and_fit_model
 import functools
-
+from ax.plot.slice import plot_slice
+from ax.plot.scatter import interact_fitted
 
 #-----------------------------Initialization-----------------------------------------------------------------------------------------
 # Read the CSV file
@@ -57,6 +63,9 @@ sample_dicts = [
 
 distance_list = []
 
+trials_count = 40
+random = 10
+
 #-----------------------------Functions-----------------------------------------------------------------------------------------
 # Assume you precompute these from your sample_dicts
 
@@ -72,13 +81,10 @@ minvalues = {
     "Ar": min([sample["Ar"] for sample in sample_dicts]),
     "Vf": min([sample["Vf"] for sample in sample_dicts]),
 }
+
 def normalize(x, key):  
+    return (x - minvalues[key]) / (maxvalues[key] - minvalues[key])
     
-    return x
-
-
-
-
 
 def get_nearest_sample(candidate):
     nearest_sample = None
@@ -86,7 +92,6 @@ def get_nearest_sample(candidate):
     
     for sample in sample_dicts:
         # Distance calculation based on Kr, Rs, Ar, and Vf
-
         distance = np.sqrt(
             (normalize(candidate['Kr'], "Kr") - normalize(sample["Kr"], "Kr"))**2 +
             (normalize(candidate['Rs'], "Rs") - normalize(sample["Rs"], "Rs"))**2 +
@@ -109,25 +114,35 @@ def get_nearest_sample(candidate):
 gs = GenerationStrategy(
     steps=[
         GenerationStep(  # Initialization step
-            # Which model to use for this step
             model=Models.SOBOL,
-            # How many generator runs (each of which is then made a trial)
-            # to produce with this step
-            num_trials=5,
-            # How many trials generated from this step must be `COMPLETED`
-            # before the next one
+            num_trials=random,
             min_trials_observed=5,
         ),
-        # figure out how the model is picked in the stand
         GenerationStep(  # BayesOpt step
             model=Models.BOTORCH_MODULAR,
             # No limit on how many generator runs will be produced
-            num_trials=-1,
+            num_trials=math.ceil((trials_count-random)/2),
             model_kwargs={  # Kwargs to pass to `BoTorchModel.__init__`
                 "surrogate_spec": SurrogateSpec(
                     model_configs=[ModelConfig(botorch_model_class=SingleTaskGP)]
                 ),
-                "botorch_acqf_class": qLogNoisyExpectedImprovement,
+                "botorch_acqf_class": UpperConfidenceBound, #adjustable for exploration via  beta
+                
+            },
+            model_gen_kwargs={
+        "acquisition_function_options": {
+            "beta": 10.0,  # Adjust this value for exploration
+                },
+            }
+        ),
+        GenerationStep(  # BayesOpt step
+            model=Models.BOTORCH_MODULAR,
+            num_trials=round((trials_count-random)/2),
+            model_kwargs={ 
+                "surrogate_spec": SurrogateSpec(
+                    model_configs=[ModelConfig(botorch_model_class=SingleTaskGP)]
+                ),
+                "botorch_acqf_class": ProbabilityOfImprovement, #Maximzation of the current best solution
             },
         ),
     ]
@@ -135,14 +150,8 @@ gs = GenerationStrategy(
 # multiple acqu fuc starting with 'exploration' later with 'exploitation' 
 
 #-----------------------------Experiment-----------------------------------------------------------------------------------------
-
-
-# Create Ax experiment
-ax_client = AxClient(generation_strategy=gs)
-
-ax_client.create_experiment(
-    name="k_mean_optimization",
-    parameters=[{
+"""
+    [{
         "name": "Kr",
         "type": "choice",
         "values": [5.0, 10.0, 15.0, 25.0,40.0,55.0,70.0,100.0],
@@ -167,25 +176,59 @@ ax_client.create_experiment(
         "is_ordered": True,
         "value_type": "float",
     }],
+"""
+
+# Create Ax experiment
+ax_client = AxClient(generation_strategy=gs)
+
+ax_client.create_experiment(
+    name="k_mean_optimization",
+    parameters=[{
+        "name": "Kr",
+        "type": "range",
+        "bounds": [5.0, 100.0],
+
+        "value_type": "float",
+    }, {
+        "name": "Rs",
+        "type": "choice",
+        "values": [10000.0, 0.0001, 1e-06, 0.01, 100.0, 1000000.0, 100000000.0, 10000000000.0],
+        "is_ordered": True,
+        "value_type": "float",
+    },  {
+        "name": "Ar",
+        "type": "range",
+        "bounds": [1.0, 6.0],
+
+        "value_type": "float",
+    }, {
+        "name": "Vf",
+        "type": "range",
+        "bounds": [5.0,60.0],
+
+        "value_type": "float",
+    }],
     # not range but list type
 
 objectives={"k_mean": ObjectiveProperties(minimize=True)}
 )
 
-for i in range(50):
+for i in range(trials_count):
     params, trial_index = ax_client.get_next_trial()
     para = get_nearest_sample(params)
     trial = ax_client.get_trial(trial_index)
     save = para.copy() 
     del save["k_mean"]
     gs = ax_client.generation_strategy
-    print([step.model for step in gs._steps])
     trial.arm._parameters.update(save)
     ax_client.complete_trial(trial_index=trial_index, raw_data={"k_mean": ( para["k_mean"] , 0.0)})
 
 
 
 #-----------------------------Plotting-----------------------------------------------------------------------------------------
+
+aqc_func_name_list = ["ProbabilityOfImprovement", "UpperConfidenceBound beta = 1", "Random"]
+    
 df = ax_client.get_trials_data_frame()
 df.to_csv("trials_data.csv", index=True)
 
@@ -203,8 +246,8 @@ generation_strategy = ax_client.generation_strategy
 strategy_changes = []
 trial_count = 0
 for step in generation_strategy._steps:
-   
-    strategy_changes.append((trial_count, step.model_kwargs["botorch_acqf_class"]))
+    strategy_changes.append((trial_count,aqc_func_name_list.pop(-1)))
+    
     if step.num_trials is None:
         break
     trial_count += step.num_trials
@@ -213,31 +256,53 @@ for idx, model in strategy_changes:
     fig.add_vline(
         x=idx,
         line=dict(color="red", dash="dash"),
-        annotation_text=f"Strategy: {model}",
+        annotation_text=f"Acqu Func.: {model}",
         annotation_position="top right"
     )
     fig.update_yaxes(tickformat=".1e")  # or ".2e", ".0e" depending on your precision needs
 
 fig.show()
 
+# Get Ax figures (don't use render yet)
+ax_figs = [
+    fig,
+    interact_fitted(model=ax_client.generation_strategy.model, rel=False),
+    interact_contour(model=ax_client.generation_strategy.model, metric_name="k_mean"),
+    plot_slice(model=ax_client.generation_strategy.model, metric_name="k_mean", param_name="Kr"),
+    plot_slice(model=ax_client.generation_strategy.model, metric_name="k_mean", param_name="Rs"),
+    plot_slice(model=ax_client.generation_strategy.model, metric_name="k_mean", param_name="Ar"),
+    plot_slice(model=ax_client.generation_strategy.model, metric_name="k_mean", param_name="Vf"),
+]
+figs = [go.Figure(fig.data) for fig in ax_figs]
 
-# Get contour data
-# figure out which plot displayes the most information 
-# k_mean vs iteration
-render(ax_client.get_optimization_trace())
-render(ax_client.get_contour_plot())
-render(ax_client.get_contour_plot(param_x="Kr", param_y="Rs", metric_name="k_mean"))
-render(ax_client.get_contour_plot(param_x="Ar", param_y="Vf", metric_name="k_mean"))
-render(ax_client.get_contour_plot(param_x="Kr", param_y="Vf", metric_name="k_mean"))
+# Generate combined HTML using Plotly (Ax uses Plotly internally)
+html_parts = [
+    plot(fig, include_plotlyjs=i==0, output_type="div") for i, fig in enumerate(figs)
+]
 
-# test diffrent visualization methods
+html_content = f"""
+<html>
+<head><title>Ax Plots</title></head>
+<body>
+{''.join(html_parts)}
+</body>
+</html>
+"""
+
+# Save to a file
+with open("ax_combined_report.html", "w") as f:
+    f.write(html_content)
+
+# Open in Windows Firefox (from WSL)
+os.system(r"/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe ax_combined_report.html")
+
+
+
+# test diffrent visualization methods -
 # How to use plotly
 
 
-
 # task:
-# mult acqu fuc
-# diffrent kind of ploting 
-# k_mean vs iterations
-# correct implementation of normalization
-# mail with github and calendar
+# adding points to the slice plot
+#figure out the beta
+# testing with breakpoint
